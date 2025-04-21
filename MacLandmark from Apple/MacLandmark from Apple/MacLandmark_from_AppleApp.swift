714b7269363d9344b74197fab683616a6f862f74
Israel Manzo <israelmanzo814@gmail.com>
2025-04-21 10:09:22 -0400

    added a new project for macOS

---
+++ MacLandmark from Apple/MacLandmark from Apple/MacLandmark_from_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmark_from_AppleApp.swift
+//  MacLandmark from Apple
+//
+//  Created by Israel Manzo on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmark_from_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
