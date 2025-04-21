c8bf249ab4f0a81234266c05b177301e00ef8a08
Israel Manzo <israelmanzo814@gmail.com>
2025-04-21 12:36:04 -0400

    updated mac os app

--- MacLandmark from Apple/MacLandmark from Apple/ContentView.swift
+++ MacLandmark from Apple/MacLandmark from Apple/ContentView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text("Hello SwiftUI App on macOS")
+                .padding()
+                .font(.title)
+            
+            Image(systemName: "applelogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .padding()
         }
         .padding()
     }
