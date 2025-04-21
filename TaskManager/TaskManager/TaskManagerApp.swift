fd1b264cf26cb42f187ac0155dac0f714855a701
Israel Manzo <israelmanzo814@gmail.com>
2025-04-21 16:57:20 -0400

    add new project

---
+++ TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Israel Manzo on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
