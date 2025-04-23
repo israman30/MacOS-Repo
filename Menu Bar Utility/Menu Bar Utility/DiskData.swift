52216e14769cf342df0e140e24a16ddc7f604b25
Israel Manzo <israelmanzo814@gmail.com>
2025-04-23 19:31:59 -0400

    added disk data model

---
+++ Menu Bar Utility/Menu Bar Utility/DiskData.swift
@@ -0,0 +1,17 @@
+//
+//  DiskData.swift
+//  Menu Bar Utility
+//
+//  Created by Israel Manzo on 4/23/25.
+//
+
+import Foundation
+
+struct DiskData {
+    let fileSystemURL: String
+    let size: Int64
+    let used: Int64
+    let available: Int64
+    let capcity: Int
+    let mountPoint: String
+}
