006365b3127c481ab04ed8e4e80f120efd80e17c
Israel Manzo <israelmanzo814@gmail.com>
2025-05-12 09:27:32 -0400

    added new project for notes

---
+++ Notes App/Notes App/Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppApp.swift
+//  Notes App
+//
+//  Created by Israel Manzo on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
